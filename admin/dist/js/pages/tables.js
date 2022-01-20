f0a866dbcbd4a70b090ef2d74fc8fe94491f4de4
Samuel Miller <millers51@udayton.edu>
2022-01-20 15:15:31 -0500

    Testing Dropdown for Admin Page Users

--- admin/dist/js/pages/tables.js
+++ admin/dist/js/pages/tables.js
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 // Once everything is loaded, get the data
-window.addEventListener("load", function(){
+window.addEventListener("load", function () {
     refreshTable()
 });
 
@@ -34,74 +34,75 @@ function refreshTable() {
     xhr.setRequestHeader("x-access-token", localStorage.getItem('swj-access'));
 
     xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4) {
-        const data = JSON.parse(xhr.responseText);
-        console.log(data[0])
+        if (xhr.readyState === 4) {
+            const data = JSON.parse(xhr.responseText);
+            console.log(data[0])
 
-        $("#main-table").DataTable(
-            {
-            data: data,
-            columns: [
-                { data: 'date_range' },
-                { data: 'surname' },
-                { data: 'firstname' },
-                { data: 'proposer' },
-                { data: 'city' },
-                { data: 'joined' }
-            ],
-            "responsive": true, 
-            "lengthChange": false, 
-            "autoWidth": false,
-            "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
-        }).buttons().container().appendTo('#main-table_wrapper .col-md-6:eq(0)');
-    }};
-    xhr.send()
-}
-
-//
-/* Formatting function for row details - modify as you need */
-function format ( d ) {
-    // `d` is the original data object for the row
-    return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">'+
-        '<tr>'+
-            '<td>Full name:</td>'+
-            '<td>'+d.name+'</td>'+
-        '</tr>'+
-    '</table>';
-}
- 
-$(document).ready(function() {
-    var table = $('#main-table').DataTable( {
-        "ajax": "../ajax/data/objects.txt",
-        "columns": [
-            {
-                "className":      'dt-control',
-                "orderable":      false,
-                "data":           null,
-                "defaultContent": ''
-            },
-            { "data": "name" },
-            { "data": "position" },
-            { "data": "office" },
-            { "data": "salary" }
-        ],
-        "order": [[1, 'asc']]
-    } );
-     
-    // Add event listener for opening and closing details
+            var table = $("#main-table").DataTable(
+                {
+                    data: data,
+                    columns: [
+                        { data: 'date_range' },
+                        { data: 'surname' },
+                        { data: 'firstname' },
+                        { data: 'proposer' },
+                        { data: 'city' },
+                        { data: 'joined' }
+                    ],
+                    "responsive": true,
+                    "lengthChange": false,
+                    "autoWidth": false,
+                    "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+                }).buttons().container().appendTo('#main-table_wrapper .col-md-6:eq(0)');
+        }
+    };
     $('#main-table tbody').on('click', 'td.dt-control', function () {
         var tr = $(this).closest('tr');
-        var row = table.row( tr );
- 
-        if ( row.child.isShown() ) {
+        var row = table.row(tr);
+
+        if (row.child.isShown()) {
             // This row is already open - close it
             row.child.hide();
             tr.removeClass('shown');
         }
         else {
             // Open this row
-            row.child( format(row.data()) ).show();
+            row.child(format(row.data())).show();
             tr.addClass('shown');
         }
-    } );
-} );
+    });
+    xhr.send()
+}
+
+//
+/* Formatting function for row details - modify as you need */
+function format(d) {
+    // `d` is the original data object for the row
+    return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">' +
+        '<tr>' +
+        '<td>Full name:</td>' +
+        '<td>' + 'placeholder!' + '</td>' +
+        '</tr>' +
+        '</table>';
+}
+
+// $(document).ready(function () {
+// var table = $('#main-table').DataTable({
+//     "ajax": "../ajax/data/objects.txt",
+//     "columns": [
+//         {
+//             "className": 'dt-control',
+//             "orderable": false,
+//             "data": null,
+//             "defaultContent": ''
+//         },
+//         { "data": "name" },
+//         { "data": "position" },
+//         { "data": "office" },
+//         { "data": "salary" }
+//     ],
+//     "order": [[1, 'asc']]
+// });
+
+
+// });
