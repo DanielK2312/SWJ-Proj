0e28cb5b97fb19aec0d791aeb679a0b0dc84e42a
Samuel-Miller-HCL <86303438+Samuel-Miller-HCL@users.noreply.github.com>
2022-01-23 13:30:30 -0500

    Continued testing of dropdown

--- admin/dist/js/pages/tables.js
+++ admin/dist/js/pages/tables.js
@@ -54,57 +54,7 @@ function refreshTable() {
                     "autoWidth": false,
                     "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
                 }).buttons().container().appendTo('#main-table_wrapper .col-md-6:eq(0)');
-
-                
-                // $('#main-table tbody').on('click', 'td.dt-control', function () {
-                //     var tr = $(this).closest('tr');
-                //     var row = table.row(tr);
-            
-                //     if (row.child.isShown()) {
-                //         // This row is already open - close it
-                //         row.child.hide();
-                //         tr.removeClass('shown');
-                //     }
-                //     else {
-                //         // Open this row
-                //         row.child(format(row.data())).show();
-                //         tr.addClass('shown');
-                //     }
-                // });
         }
     };
     xhr.send()
 }
-
-//
-/* Formatting function for row details - modify as you need */
-function format(d) {
-    // `d` is the original data object for the row
-    return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">' +
-        '<tr>' +
-        '<td>Full name:</td>' +
-        '<td>' + d.name + '</td>' +
-        '</tr>' +
-        '</table>';
-}
-
-// $(document).ready(function () {
-//     var table = $('#main-table').DataTable({
-//         data: data,
-//         columns: [
-//             { data: 'date_range' },
-//             { data: 'surname' },
-//             { data: 'firstname' },
-//             { data: 'proposer' },
-//             { data: 'city' },
-//             { data: 'joined' }
-//         ],
-//         "responsive": true,
-//         "lengthChange": false,
-//         "autoWidth": false,
-//         "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
-//     });
-
-//     // Add event listener for opening and closing details
-    
-// });
