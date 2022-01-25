821f6d2750a0b10b81c3a7496c783adbb41f934d
Samantha <adrians1@udayton.edu>
2022-01-25 16:05:17 -0500

    Dropdown Format Test 1

--- admin/dist/js/pages/tables.js
+++ admin/dist/js/pages/tables.js
@@ -62,21 +62,21 @@ $(document).ready(function () {
 /* Formatting function for row details - modify as you need */
 function format(d) {
     // `d` is the original data object for the row
-    return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">' +
+    let memberInfo = '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">' +
         '<tr>' +
         '<td>Full name:</td>' +
         '<td>' + d.prefix + " " + d.firstname + " " + d.surname + '</td>' +
-        '</tr>' +
+        '</tr>';
 
-        '<tr>' +
-        '<td>Extension number:</td>' +
-        // '<td>' + d.extn + '</td>' +
-        '</tr>' +
+        if (d.pen_name != null){
+            memberInfo += '<tr>' +
+                '<td>Extension number:</td>' +
+                '<td>' + d.extn + '</td>' +
+                '</tr>';
+        }
         
-        '<tr>' +
-        '<td>Extra info:</td>' +
-        '<td>And any further details here (images etc)...</td>' +
-        '</tr>' +
-        '</table>';
+        memberInfo += '</table>';
+
+        return memberInfo;
 }
 
