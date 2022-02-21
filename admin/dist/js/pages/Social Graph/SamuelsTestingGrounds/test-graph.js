d13bf7d0b8184c0d67fefbea78abc880d1f5e851
Samuel Miller <millers51@udayton.edu>
2022-02-21 12:35:41 -0500

    Updated version of Examples ready

--- admin/dist/js/pages/Social Graph/SamuelsTestingGrounds/test-graph.js
+++ admin/dist/js/pages/Social Graph/SamuelsTestingGrounds/test-graph.js
@@ -1,4 +1,5 @@
-
+// let json = require('./data.json');
+// console.log(json, 'the json obj');
 // Initialise sigma:
 var s = new sigma(
     {
@@ -18,14 +19,16 @@ var s = new sigma(
 // Create a graph object
 var graph = {
     nodes: [
-        { id: "n0", label: "A node", x: 0, y: 0, size: 3, color: '#008cc2' },
-        { id: "n1", label: "Another node", x: 3, y: 1, size: 2, color: '#008cc2' },
-        { id: "n2", label: "And a last one", x: 1, y: 3, size: 1, color: '#E57821' }
+        { id: "n0", label: "Samuel", x: 3, y: 2, size: 3, color: '#008cc2' },
+        { id: "n1", label: "Gabe", x: 2, y: 1, size: 2, color: '#008cc2' },
+        { id: "n2", label: "Samantha", x: 4, y: 2, size: 1, color: '#E57821' },
+        { id: "n3", label: "Daniel", x: 3, y: 4 1, size: 4, color: '#E57821' }
     ],
     edges: [
         { id: "e0", source: "n0", target: "n1", color: '#282c34', type: 'line', size: 0.5 },
         { id: "e1", source: "n1", target: "n2", color: '#282c34', type: 'curve', size: 1 },
-        { id: "e2", source: "n2", target: "n0", color: '#FF0000', type: 'line', size: 2 }
+        { id: "e2", source: "n2", target: "n0", color: '#FF0000', type: 'line', size: 2 },
+        { id: "e3", source: "n3", target: "n0", color: '#black', type: 'line', size: 1 }
     ]
 }
 
