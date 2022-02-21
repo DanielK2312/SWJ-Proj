5adcad3e16c2243ba00330e7e51523202d3c64e4
Gabe Hoban <gabehoban1@posteo.de>
2022-02-21 10:11:09 -0500

    Add new API routes PART II

--- backend/dist/routes/personRouter.js
+++ backend/dist/routes/personRouter.js
@@ -13,18 +13,69 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const mongoose_1 = __importDefault(require("mongoose"));
 const personModel_1 = __importDefault(require("../models/personModel"));
 const personRouter = express_1.default.Router();
-personRouter.get('/search/:surname', (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
-    personModel_1.default.find({ "surname": { "$regex": req.params.surname, "$options": "i" } })
-        .then((result) => {
-        res.status(200).json(result);
-    })
-        .catch((err) => {
-        res.status(500).json({ err });
-    });
+/**
+ * Gets one person from the database by surname.
+ * @remarks
+ * GET /api/v1/person/byname/:surname
+ *
+ * @returns <JSON> { Person[] }
+ */
+personRouter.get('/byname/:surname', (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    if (req.params.surname) {
+        personModel_1.default.find({ "surname": { "$regex": req.params.surname, "$options": "i" } })
+            .then((result) => {
+            res.status(200).json(result);
+        })
+            .catch((err) => {
+            res.status(500).json({ err });
+        });
+    }
 }));
+/**
+ * Gets one person from the database by position.
+ * @remarks
+ * GET /api/v1/person/byposition/:position
+ *
+ * @returns <JSON> { Person[] }
+ */
+personRouter.get('/byposition/:position', (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    if (req.params.position) {
+        personModel_1.default.find({ "position": { "$regex": req.params.position, "$options": "i" } })
+            .then((result) => {
+            res.status(200).json(result);
+        })
+            .catch((err) => {
+            res.status(500).json({ err });
+        });
+    }
+}));
+/**
+ * Gets one person from the database by date_range.
+ * @remarks
+ * GET /api/v1/person/bydate/:date_range
+ *
+ * @returns <JSON> { Person[] }
+ */
+personRouter.get('/bydate/:date_range', (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    if (req.params.date_range) {
+        personModel_1.default.find({ "date_range": req.params.date_range })
+            .then((result) => {
+            res.status(200).json(result);
+        })
+            .catch((err) => {
+            res.status(500).json({ err });
+        });
+    }
+}));
+/**
+ * Lists all persons from the database.
+ * @remarks
+ * GET /api/v1/person/list
+ *
+ * @returns <JSON> { Person[] }
+ */
 personRouter.get('/list', (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     personModel_1.default.find({})
         .then((result) => {
@@ -35,6 +86,14 @@ personRouter.get('/list', (req, res, next) => __awaiter(void 0, void 0, void 0, 
     });
     //
 }));
+/**
+ * Creates a person from the database.
+ * @remarks
+ * POST /api/v1/person/create
+ *
+ * @param req.body.* - Fields of the person object.
+ * @returns <JSON> { Person }
+ */
 personRouter.post('/create', (req, res, next) => {
     // Surname is the only required field to create a person.
     if (req.body.surname) {
@@ -43,10 +102,46 @@ personRouter.post('/create', (req, res, next) => {
         res.status(200).json(newPerson);
     }
 });
-personRouter.get('/drop', (req, res, next) => {
-    // Surname is the only required field to create a person.
-    mongoose_1.default.connection.db.dropCollection('people', (err, result) => { console.log(err); });
-    res.status(200).json({ status: 'success' });
+/**
+ * Updates a person from the database.
+ * @remarks
+ * POST /api/v1/person/update
+ *
+ * @param req.body.id - The _id field created by MongoDB.
+ * @param req.body.updates - JSON of only updated fields
+ * @returns <JSON> { status, result } | { status, err }
+ */
+personRouter.post('/update', (req, res, next) => {
+    // ID is mongoDB _id field
+    // updates is a JSON like { surname: 'NewName' }
+    const { id, updates } = req.body;
+    const person = personModel_1.default.findById(id);
+    person.updateOne(updates)
+        .then(result => {
+        res.status(200).json({ status: 'success', result });
+    })
+        .catch(err => {
+        res.status(500).json({ status: 'error', err });
+    });
+});
+/**
+ * Deletes a person from the database.
+ * @remarks
+ * GET /api/v1/person/delete
+ *
+ * @param req.body.id - The _id field created by MongoDB.
+ * @returns <JSON> { status, result } | { status, err }
+ */
+personRouter.get('/delete', (req, res, next) => {
+    const id = req.body;
+    const person = personModel_1.default.findById(id);
+    person.deleteOne()
+        .then(result => {
+        res.status(200).json({ status: 'success', result });
+    })
+        .catch(err => {
+        res.status(500).json({ status: 'error', err });
+    });
 });
 exports.default = personRouter;
 //# sourceMappingURL=personRouter.js.map
--- backend/src/routes/personRouter.ts
+++ backend/src/routes/personRouter.ts
@@ -1,17 +1,73 @@
 import express from 'express';
-import mongoose from 'mongoose';
 import Person from '../models/personModel';
 
 const personRouter = express.Router();
-personRouter.get('/search/:surname', async (req, res, next) => {
-    Person.find({ "surname": { "$regex": req.params.surname!, "$options": "i" } })
+
+/**
+ * Gets one person from the database by surname.
+ * @remarks
+ * GET /api/v1/person/byname/:surname
+ *
+ * @returns <JSON> { Person[] }
+ */
+personRouter.get('/byname/:surname', async (req, res, next) => {
+    if (req.params.surname) {
+        Person.find({ "surname": { "$regex": req.params.surname, "$options": "i" } })
         .then((result) => {
             res.status(200).json(result);
         })
         .catch((err) => {
             res.status(500).json({ err });
         });
+    }
 });
+
+/**
+ * Gets one person from the database by position.
+ * @remarks
+ * GET /api/v1/person/byposition/:position
+ *
+ * @returns <JSON> { Person[] }
+ */
+ personRouter.get('/byposition/:position', async (req, res, next) => {
+    if (req.params.position) {
+        Person.find({ "position": { "$regex": req.params.position, "$options": "i" } })
+        .then((result) => {
+            res.status(200).json(result);
+        })
+        .catch((err) => {
+            res.status(500).json({ err });
+        });
+    }
+});
+
+/**
+ * Gets one person from the database by date_range.
+ * @remarks
+ * GET /api/v1/person/bydate/:date_range
+ *
+ * @returns <JSON> { Person[] }
+ */
+ personRouter.get('/bydate/:date_range', async (req, res, next) => {
+    if (req.params.date_range) {
+        Person.find({ "date_range": req.params.date_range })
+        .then((result) => {
+            res.status(200).json(result);
+        })
+        .catch((err) => {
+            res.status(500).json({ err });
+        });
+    }
+});
+
+
+/**
+ * Lists all persons from the database.
+ * @remarks
+ * GET /api/v1/person/list
+ *
+ * @returns <JSON> { Person[] }
+ */
 personRouter.get('/list', async (req, res, next) => {
     Person.find({})
         .then((result) => {
@@ -22,15 +78,33 @@ personRouter.get('/list', async (req, res, next) => {
         });
     //
 });
+
+/**
+ * Creates a person from the database.
+ * @remarks
+ * POST /api/v1/person/create
+ *
+ * @param req.body.* - Fields of the person object.
+ * @returns <JSON> { Person }
+ */
 personRouter.post('/create', (req, res, next) => {
     // Surname is the only required field to create a person.
     if (req.body.surname) {
-      const newPerson = new Person(req.body);
-      newPerson.save();
-      res.status(200).json(newPerson);
+        const newPerson = new Person(req.body);
+        newPerson.save()
+        res.status(200).json(newPerson);
     }
 });
 
+/**
+ * Updates a person from the database.
+ * @remarks
+ * POST /api/v1/person/update
+ *
+ * @param req.body.id - The _id field created by MongoDB.
+ * @param req.body.updates - JSON of only updated fields
+ * @returns <JSON> { status, result } | { status, err }
+ */
 personRouter.post('/update', (req, res, next) => {
     // ID is mongoDB _id field
     // updates is a JSON like { surname: 'NewName' }
@@ -38,20 +112,27 @@ personRouter.post('/update', (req, res, next) => {
     const person = Person.findById(id);
     person.updateOne(updates)
         .then(result => {
-            res.status(200).json({ status: 'success', result});
+            res.status(200).json({ status: 'success', result });
         })
         .catch(err => {
             res.status(500).json({ status: 'error', err })
         });
 });
 
+/**
+ * Deletes a person from the database.
+ * @remarks
+ * GET /api/v1/person/delete
+ *
+ * @param req.body.id - The _id field created by MongoDB.
+ * @returns <JSON> { status, result } | { status, err }
+ */
 personRouter.get('/delete', (req, res, next) => {
-    // ID is mongoDB _id field
     const id = req.body;
     const person = Person.findById(id);
     person.deleteOne()
         .then(result => {
-            res.status(200).json({ status: 'success', result});
+            res.status(200).json({ status: 'success', result });
         })
         .catch(err => {
             res.status(500).json({ status: 'error', err })
