d42b32dde4ab9152f9f3d01b50ce35c1e710ce2d
Samantha <adrians1@udayton.edu>
2022-02-27 20:31:50 -0500

    Dashboard Work 1

--- admin/dist/js/pages/dashboard.js
+++ admin/dist/js/pages/dashboard.js
@@ -1,2 +1,31 @@
+$(document).ready(function () {
+    var url = "https://swj-capstone-staging.herokuapp.com/api/v1/person/list";
 
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            const data = JSON.parse(xhr.responseText);
+            console.log(data[0])
+            //Handling member-count info box
+            document.getElementById("count-number").innerText = data.length;
+            document.getElementById("loading").style.display = "none";
+
+            //Handling user-count info box
+                //edit here after setting up domain
+            document.getElementById("loading2").style.display = "none";
+        }
+    };
+    xhr.send()
+})
+
+$(".container").mapael({
+    map : {
+        name : "world_countries"
+    }
+});
 
