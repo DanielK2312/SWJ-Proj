f25d157fb6dd38d30ddbc92104af59affeca6eaa
Gabe Hoban <hello@gabehoban.com>
2022-03-01 10:41:02 -0500

    Enable secure headers for production

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -38,6 +38,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
 // # - Security Middleware -#
+app.use(helmet())
 const allowedOrigins = ['http://localhost:3000', 'https://swj-capstone-staging.herokuapp.com'];
 const options: cors.CorsOptions = { origin: allowedOrigins };
 app.use(cors(options));
