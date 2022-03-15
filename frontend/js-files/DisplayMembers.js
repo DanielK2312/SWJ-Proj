bf41fa61e53d53b5d4f9ebaeb118a2aa7d538ea8
Daniel Kosmin <kosmind1@udayton.edu>
2022-03-15 17:02:06 -0400

    trying to create modal window for each individual, still in progress

--- frontend/js-files/DisplayMembers.js
+++ frontend/js-files/DisplayMembers.js
@@ -33,185 +33,393 @@ let allocateNames = (jsonRes) => {
     let firstName = element.firstname;
     if (surname === "A") {
       if (firstName === "") {
-        holdA.push(element.surname);
+        let obj = {
+          surname: element.surname,
+          id: element._id,
+        };
+        holdA.push(obj);
       } else {
-        let name = element.surname + ", " + element.firstname;
-        holdA.push(name);
+        let obj = {
+          firstName: element.firstname,
+          surname: element.surname,
+          id: element._id,
+        };
+        holdA.push(obj);
       }
     } else if (surname === "B") {
       if (firstName === "") {
-        holdB.push(element.surname);
+        let obj = {
+          surname: element.surname,
+          id: element._id,
+        };
+        holdB.push(obj);
       } else {
-        let name = element.surname + ", " + element.firstname;
-        holdB.push(name);
+        let obj = {
+          firstName: element.firstname,
+          surname: element.surname,
+          id: element._id,
+        };
+        holdB.push(obj);
       }
     } else if (surname === "C") {
       if (firstName === "") {
-        holdC.push(element.surname);
+        let obj = {
+          surname: element.surname,
+          id: element._id,
+        };
+        holdC.push(obj);
       } else {
-        let name = element.surname + ", " + element.firstname;
-        holdC.push(name);
+        let obj = {
+          firstName: element.firstname,
+          surname: element.surname,
+          id: element._id,
+        };
+        holdC.push(obj);
       }
     } else if (surname === "D") {
       if (firstName === "") {
-        holdD.push(element.surname);
+        let obj = {
+          surname: element.surname,
+          id: element._id,
+        };
+        holdD.push(obj);
       } else {
-        let name = element.surname + ", " + element.firstname;
-        holdD.push(name);
+        let obj = {
+          firstName: element.firstname,
+          surname: element.surname,
+          id: element._id,
+        };
+        holdD.push(obj);
       }
     } else if (surname === "E") {
       if (firstName === "") {
-        holdE.push(element.surname);
+        let obj = {
+          surname: element.surname,
+          id: element._id,
+        };
+        holdE.push(obj);
       } else {
-        let name = element.surname + ", " + element.firstname;
-        holdE.push(name);
+        let obj = {
+          firstName: element.firstname,
+          surname: element.surname,
+          id: element._id,
+        };
+        holdE.push(obj);
       }
     } else if (surname === "F") {
       if (firstName === "") {
-        holdF.push(element.surname);
+        let obj = {
+          surname: element.surname,
+          id: element._id,
+        };
+        holdF.push(obj);
       } else {
-        let name = element.surname + ", " + element.firstname;
-        holdF.push(name);
+        let obj = {
+          firstName: element.firstname,
+          surname: element.surname,
+          id: element._id,
+        };
+        holdF.push(obj);
       }
     } else if (surname === "G") {
       if (firstName === "") {
-        holdG.push(element.surname);
+        let obj = {
+          surname: element.surname,
+          id: element._id,
+        };
+        holdG.push(obj);
       } else {
-        let name = element.surname + ", " + element.firstname;
-        holdG.push(name);
+        let obj = {
+          firstName: element.firstname,
+          surname: element.surname,
+          id: element._id,
+        };
+        holdG.push(obj);
       }
     } else if (surname === "H") {
       if (firstName === "") {
-        holdH.push(element.surname);
+        let obj = {
+          surname: element.surname,
+          id: element._id,
+        };
+        holdH.push(obj);
       } else {
-        let name = element.surname + ", " + element.firstname;
-        holdH.push(name);
+        let obj = {
+          firstName: element.firstname,
+          surname: element.surname,
+          id: element._id,
+        };
+        holdH.push(obj);
       }
     } else if (surname === "I") {
       if (firstName === "") {
-        holdI.push(element.surname);
+        let obj = {
+          surname: element.surname,
+          id: element._id,
+        };
+        holdI.push(obj);
       } else {
-        let name = element.surname + ", " + element.firstname;
-        holdI.push(name);
+        let obj = {
+          firstName: element.firstname,
+          surname: element.surname,
+          id: element._id,
+        };
+        holdI.push(obj);
       }
     } else if (surname === "J") {
       if (firstName === "") {
-        holdJ.push(element.surname);
+        let obj = {
+          surname: element.surname,
+          id: element._id,
+        };
+        holdJ.push(obj);
       } else {
-        let name = element.surname + ", " + element.firstname;
-        holdJ.push(name);
+        let obj = {
+          firstName: element.firstname,
+          surname: element.surname,
+          id: element._id,
+        };
+        holdJ.push(obj);
       }
     } else if (surname === "K") {
       if (firstName === "") {
-        holdK.push(element.surname);
+        let obj = {
+          surname: element.surname,
+          id: element._id,
+        };
+        holdK.push(obj);
       } else {
-        let name = element.surname + ", " + element.firstname;
-        holdK.push(name);
+        let obj = {
+          firstName: element.firstname,
+          surname: element.surname,
+          id: element._id,
+        };
+        holdK.push(obj);
       }
     } else if (surname === "L") {
       if (firstName === "") {
-        holdL.push(element.surname);
+        let obj = {
+          surname: element.surname,
+          id: element._id,
+        };
+        holdL.push(obj);
       } else {
-        let name = element.surname + ", " + element.firstname;
-        holdL.push(name);
+        let obj = {
+          firstName: element.firstname,
+          surname: element.surname,
+          id: element._id,
+        };
+        holdL.push(obj);
       }
     } else if (surname === "M") {
       if (firstName === "") {
-        holdM.push(element.surname);
+        let obj = {
+          surname: element.surname,
+          id: element._id,
+        };
+        holdM.push(obj);
       } else {
-        let name = element.surname + ", " + element.firstname;
-        holdM.push(name);
+        let obj = {
+          firstName: element.firstname,
+          surname: element.surname,
+          id: element._id,
+        };
+        holdM.push(obj);
       }
     } else if (surname === "N") {
       if (firstName === "") {
-        holdN.push(element.surname);
+        let obj = {
+          surname: element.surname,
+          id: element._id,
+        };
+        holdN.push(obj);
       } else {
-        let name = element.surname + ", " + element.firstname;
-        holdN.push(name);
+        let obj = {
+          firstName: element.firstname,
+          surname: element.surname,
+          id: element._id,
+        };
+        holdN.push(obj);
       }
     } else if (surname === "O") {
       if (firstName === "") {
-        holdO.push(element.surname);
+        let obj = {
+          surname: element.surname,
+          id: element._id,
+        };
+        holdO.push(obj);
       } else {
-        let name = element.surname + ", " + element.firstname;
-        holdO.push(name);
+        let obj = {
+          firstName: element.firstname,
+          surname: element.surname,
+          id: element._id,
+        };
+        holdO.push(obj);
       }
     } else if (surname === "P") {
       if (firstName === "") {
-        holdP.push(element.surname);
+        let obj = {
+          surname: element.surname,
+          id: element._id,
+        };
+        holdP.push(obj);
       } else {
-        let name = element.surname + ", " + element.firstname;
-        holdP.push(name);
+        let obj = {
+          firstName: element.firstname,
+          surname: element.surname,
+          id: element._id,
+        };
+        holdP.push(obj);
       }
     } else if (surname === "Q") {
       if (firstName === "") {
-        holdQ.push(element.surname);
+        let obj = {
+          surname: element.surname,
+          id: element._id,
+        };
+        holdQ.push(obj);
       } else {
-        let name = element.surname + ", " + element.firstname;
-        holdQ.push(name);
+        let obj = {
+          firstName: element.firstname,
+          surname: element.surname,
+          id: element._id,
+        };
+        holdQ.push(obj);
       }
     } else if (surname === "R") {
       if (firstName === "") {
-        holdR.push(element.surname);
+        let obj = {
+          surname: element.surname,
+          id: element._id,
+        };
+        holdR.push(obj);
       } else {
-        let name = element.surname + ", " + element.firstname;
-        holdR.push(name);
+        let obj = {
+          firstName: element.firstname,
+          surname: element.surname,
+          id: element._id,
+        };
+        holdR.push(obj);
       }
     } else if (surname === "S") {
       if (firstName === "") {
-        holdS.push(element.surname);
+        let obj = {
+          surname: element.surname,
+          id: element._id,
+        };
+        holdS.push(obj);
       } else {
-        let name = element.surname + ", " + element.firstname;
-        holdS.push(name);
+        let obj = {
+          firstName: element.firstname,
+          surname: element.surname,
+          id: element._id,
+        };
+        holdS.push(obj);
       }
     } else if (surname === "T") {
       if (firstName === "") {
-        holdT.push(element.surname);
+        let obj = {
+          surname: element.surname,
+          id: element._id,
+        };
+        holdT.push(obj);
       } else {
-        let name = element.surname + ", " + element.firstname;
-        holdT.push(name);
+        let obj = {
+          firstName: element.firstname,
+          surname: element.surname,
+          id: element._id,
+        };
+        holdT.push(obj);
       }
     } else if (surname === "U") {
       if (firstName === "") {
-        holdU.push(element.surname);
+        let obj = {
+          surname: element.surname,
+          id: element._id,
+        };
+        holdU.push(obj);
       } else {
-        let name = element.surname + ", " + element.firstname;
-        holdU.push(name);
+        let obj = {
+          firstName: element.firstname,
+          surname: element.surname,
+          id: element._id,
+        };
+        holdU.push(obj);
       }
     } else if (surname === "V") {
       if (firstName === "") {
-        holdV.push(element.surname);
+        let obj = {
+          surname: element.surname,
+          id: element._id,
+        };
+        holdV.push(obj);
       } else {
-        let name = element.surname + ", " + element.firstname;
-        holdV.push(name);
+        let obj = {
+          firstName: element.firstname,
+          surname: element.surname,
+          id: element._id,
+        };
+        holdV.push(obj);
       }
     } else if (surname === "W") {
       if (firstName === "") {
-        holdW.push(element.surname);
+        let obj = {
+          surname: element.surname,
+          id: element._id,
+        };
+        holdW.push(obj);
       } else {
-        let name = element.surname + ", " + element.firstname;
-        holdW.push(name);
+        let obj = {
+          firstName: element.firstname,
+          surname: element.surname,
+          id: element._id,
+        };
+        holdW.push(obj);
       }
     } else if (surname === "X") {
       if (firstName === "") {
-        holdX.push(element.surname);
+        let obj = {
+          surname: element.surname,
+          id: element._id,
+        };
+        holdX.push(obj);
       } else {
-        let name = element.surname + ", " + element.firstname;
-        holdX.push(name);
+        let obj = {
+          firstName: element.firstname,
+          surname: element.surname,
+          id: element._id,
+        };
+        holdX.push(obj);
       }
     } else if (surname === "Y") {
       if (firstName === "") {
-        holdY.push(element.surname);
+        let obj = {
+          surname: element.surname,
+          id: element._id,
+        };
+        holdY.push(obj);
       } else {
-        let name = element.surname + ", " + element.firstname;
-        holdY.push(name);
+        let obj = {
+          firstName: element.firstname,
+          surname: element.surname,
+          id: element._id,
+        };
+        holdY.push(obj);
       }
     } else if (surname === "Z") {
       if (firstName === "") {
-        holdZ.push(element.surname);
+        let obj = {
+          surname: element.surname,
+          id: element._id,
+        };
+        holdZ.push(obj);
       } else {
-        let name = element.surname + ", " + element.firstname;
-        holdZ.push(name);
+        let obj = {
+          firstName: element.firstname,
+          surname: element.surname,
+          id: element._id,
+        };
+        holdZ.push(obj);
       }
     }
   });
@@ -227,11 +435,64 @@ let createDynamicLinks = (list, firstLetter) => {
     // document.getElementById(
     //   firstLetter
     // ).innerHTML += `<a href="url">${element}</a><br>`;
-    document.getElementById(
-      firstLetter
-    ).innerHTML += `<button id="${element}" href="url">${element}</button><br>`;
+    // document.getElementById(
+    //   firstLetter
+    // ).innerHTML += `<button onclick="renderModal('${element}')" id="${element}">${element}</button><br>`;
+    let button = document.createElement("button");
+    let br = document.createElement("br");
+    button.innerHTML = element.surname + ", " + element.firstName;
+    button.onclick = () => {
+      createModal(element.surname, element.id);
+    };
+    document.getElementById(firstLetter).appendChild(button);
+    document.getElementById(firstLetter).appendChild(br);
   });
 };
+
+// #TODO currently here trying to solve issue
+let createModal = (surname, id) => {
+  url = "https://swj-capstone.herokuapp.com/api/v1/person/byname/" + surname;
+
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", url);
+
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      let jsonRes = xhr.responseText;
+      console.log(jsonRes);
+
+      jsonRes.forEach((element) => {
+        if (element._id == id) {
+          let x = element;
+          console.log(x);
+        }
+      });
+    }
+    //   // display main modal window
+    //   $("#person-modal").modal({ backdrop: "static", keyboard: false });
+    //   $("#person-modal").modal("show");
+    //   // all data is loaded
+    //   let jsonRes = xhr.responseText;
+    //   // process string received from xhr response into object
+    //   jsonRes = JSON.parse(jsonRes);
+    //   console.log(typeof jsonRes);
+    //   // takes in xhr response, returns array of objects to process
+    //   processXhrResponse(jsonRes);
+    //   // loops through array of objects and creates a button for each of them on the main modal window
+    //   processPersonInfoArray(personInfo);
+    //   // creates dynamic modal window for each individual with a button created above
+    //   createDynamicModals(personInfo);
+    //   // handles manual triggers for each modal window
+    //   manualDynamicModalTriggers(personInfo);
+    // }
+  };
+  xhr.send();
+};
+
+renderModal = (x) => {};
 
 window.addEventListener("load", () => {
   url = "https://swj-capstone.herokuapp.com/api/v1/person/list";
