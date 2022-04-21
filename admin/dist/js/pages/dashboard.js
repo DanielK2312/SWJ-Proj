4f322712a62aa6191225a59a8ab092b724c20a15
Gabe Hoban <hello@gabehoban.com>
2022-04-21 04:03:31 -0400

    Add analytics to dashboard

--- admin/dist/js/pages/dashboard.js
+++ admin/dist/js/pages/dashboard.js
@@ -1,25 +1,86 @@
 $(document).ready(function () {
-    var url = "https://swj1894.org/api/v1/analytics/list";
-
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", url);
-
-    xhr.setRequestHeader("Accept", "application/json");
-    xhr.setRequestHeader("Content-Type", "application/json");
-
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4) {
-            const data = JSON.parse(xhr.responseText);
-            console.log(data[0])
-            //Handling member-count info box
-            document.getElementById("count-number").innerText = data.length;
-            document.getElementById("loading").style.display = "none";
-
-            //Handling user-count info box
-                //edit here after setting up domain
-            document.getElementById("loading2").style.display = "none";
-        }
-    };
-    xhr.send()
+    loadNumbers();
+    loadViews();
 })
 
+const loadNumbers = () => {
+  var url = "https://swj1894.org/api/v1/person/list";
+  // var url = "http://localhost:3000/api/v1/person/list";
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+          const data = JSON.parse(xhr.responseText);
+          const length = data.length
+          let personCount = 0
+          for(let i=0;i<length;i++) {
+            personCount++
+          }
+          document.getElementById("count-number").innerText = personCount;
+      }
+  };
+  xhr.send()
+}
+
+const loadViews = () => {
+  var url = "https://swj1894.org/api/v1/analytics/list";
+  // var url = "http://localhost:3000/api/v1/analytics/list";
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+          const data = JSON.parse(xhr.responseText);
+          document.getElementById("user-number").innerText = data.monthly;
+
+          const length = data.data.length
+          let dates = []
+          let views = []
+          for(let i=0; i<length; i++) {
+            dates.push(data.data[i].dimensions.date)
+            views.push(data.data[i].sum.pageViews)
+          }
+
+          var ctx = $("#line-chartcanvas");
+          const chartData = {labels: dates, datasets: [
+            {
+              label: "Page Views",
+              data: views,
+              backgroundColor: "grey",
+              borderColor: "lightblue",
+              fill: false,
+              lineTension: 0,
+              radius: 2
+            }]}
+            var options = {
+              responsive: true
+            };
+            var chart = new Chart(ctx, {
+              type: "line",
+              data: chartData,
+              options: options
+            });
+      }
+  };
+  xhr.send()
+}
+
+// const config = {
+//   type: 'line',
+//   data: data,
+//   options: {
+//     responsive: true,
+//     plugins: {
+//       legend: {
+//         position: 'top',
+//       },
+//       title: {
+//         display: true,
+//         text: 'Chart.js Line Chart'
+//       }
+//     }
+//   },
+// };
